91086aa410a99b75e5e204e8d087d8e0fe693143
delphine ade nchang <delphinenchang4@gmail.com>
2023-11-15 19:28:54 -0600

    added ec2 module

--- main.tf
+++ main.tf
@@ -10,6 +10,12 @@ module "rds" {
   private_subnet2 = module.vpc.private_subnet2_id
   vpc_id = module.vpc.vpc_id
   vpc_cidr = "10.0.0.0/16"
-  
+ 
 }
 
+module "ec2" {
+  source = "./ec2"
+  subnet_id = module.vpc.public_subnet1_id
+  vpc_id = module.vpc.vpc_id
+  tags = local.project_tags
+}
